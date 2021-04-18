04961090835cff01b6ef7ce2ebaf89b5ce9dbf36
nathanlarocque93 <nathan.larocque14@gmail.com>
2021-04-18 14:15:50 -0400

    addig final project

--- project-part1.sql
+++ project-part1.sql
@@ -1,8 +1,8 @@
 -- Step 1: (5 points) Create and use a database called 'project01'
 -- Ensure you check first if it already exists
 -- CREATE YOUR DATABASE AND SET IT TO BE USED BELOW THIS LINE --
-
-
+CREATE DATABASE IF NOT EXISTS project01;
+USE project01;
 -- Step 2: (30 points) Create a table called 'supers' (3 points) with the following fields
 -- Ensure you use logical data types
 -- Ensure you use the NULL and NOT NULL logically
@@ -20,6 +20,16 @@
 -- * HINT: Only 2 of the data types are strings...
 
 -- CREATE YOUR TABLE BELOW THIS LINE --
-
+CREATE TABLE supers (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+date_of_birth DATE NOT NULL,
+alias VARCHAR(30) NOT NULL,
+active BOOLEAN NOT NULL,
+hero BOOLEAN NOT NULL,
+created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+update_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
 
 -- TOTAL POINTS POSSIBLE: 35
